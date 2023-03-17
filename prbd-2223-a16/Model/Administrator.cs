9ccda0c86a9b9bb36e37dc473d3092b2df6b2f0d
alberich ravier <albe.ravier@hotmail.fr>
2023-03-17 17:30:27 +0100

    ajout model : admin, user et fichier puml

---
+++ prbd-2223-a16/Model/Administrator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPoll.Model;
+
+public class Administrator : User {
+
+    protected Administrator() {
+        Role = Role.Administrator;
+    }
+
+    public Administrator(string pseudo, string mail, string password)
+        : base(pseudo, mail, password) { 
+        Role = Role.Administrator;
+    }
+}
+
--- prbd-2223-a16/Model/User.cs
+++ prbd-2223-a16/Model/User.cs
@@ -1,8 +1,28 @@
-﻿using PRBD_Framework;
+﻿using System.DirectoryServices.ActiveDirectory;
+using PRBD_Framework;
 
 namespace MyPoll.Model;
 
+public enum Role {
+    Member = 1,
+    Administrator = 2,
+}
 public class User : EntityBase<MyPollContext> {
+
     public int UserId { get; set; }
     public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public Role Role { get; protected set; } = Role.Member;
+
+    public User(string name, string email, string password) {
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+    public User() { }
+
 }
